e2ab56ae40efa1032d955ca082e5273833db9517
AllaVict <allavict@ukr.net>
2023-06-04 18:00:33 +0200

    homework

--- src/homework8/L7_Algorithms_HW.java
+++
@@ -1,5 +0,0 @@
-package homework8;
-
-public class L7_Algorithms_HW {
-
-    }
--- src/homework8/L8_Algorithms_HW.java
+++ src/homework8/L8_Algorithms_HW.java
@@ -1,5 +1,8 @@
 package homework8;
 
 public class L8_Algorithms_HW {
+    public static void main(String[] args) {
+
+    }
 
     }
